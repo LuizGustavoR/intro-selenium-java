b50619deb307e2e36c81806c89d2a77b43a3b21c
luiz <luiz.ro.aguiar@gmail.com>
2022-11-27 17:23:44 -0300

    Fix dependency.

--- src/test/java/setup/SetupMyWebDriver.java
+++ src/test/java/setup/SetupMyWebDriver.java
@@ -3,6 +3,7 @@ package setup;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.net.MalformedURLException;
 import java.net.URL;
